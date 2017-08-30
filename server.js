28e66310df558552784e1e44fc0d153f9983927a
minalnikhade <30716958+minalnikhade@users.noreply.github.com>
2017-08-30 12:30:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-   res.send('Article one requested and will be served here by web developer Minal.'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two',function(req,res){
