f1ce238741b0b2ae2cd6536701d729ef860b930a
minalnikhade <30716958+minalnikhade@users.noreply.github.com>
2017-09-27 16:25:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
 console.log('Loaded!');
+
+//Change the text
+var element= document.getElementById('MainText');
+
+element.innerHTML= 'New Value';
+
+//Move the image
+var img = document.getElementById ('madi');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft= marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.oneclick = function (){
+    var interval = setInterval(moveRight, 100);
+};
